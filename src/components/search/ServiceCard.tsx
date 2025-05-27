f5c0cc1f98427fe5652628cb2abc105f11aa48e0
Jose Carlos Ramirez Lopez <60718287+lZood@users.noreply.github.com>
2025-05-27 16:10:57 -0700

    Deploy application to Netlify

--- src/components/search/ServiceCard.tsx
+++ src/components/search/ServiceCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Star, Heart } from 'lucide-react';
 import { Service } from '../../types';
