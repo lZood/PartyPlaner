e00f11f75c821a56fc05ad37b4547beaa09bae6c
Jose Carlos Ramirez Lopez <60718287+lZood@users.noreply.github.com>
2025-05-27 23:09:37 -0700

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
-import { ReservationProvider } from './contexts/ReservationContext'; // Asegúrate que esta importación sea correcta
+import { ReservationProvider } from './contexts/ReservationContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import MainLayout from './layouts/MainLayout';
-import ScrollToTop from './ScrollToTop'; // <--- IMPORTA EL COMPONENTE
+import ScrollToTop from './components/utils/ScrollToTop'; // <--- RUTA CORREGIDA
 
-// ... (tus imports de páginas lazy)
 const HomePage = lazy(() => import('./pages/HomePage'));
 const CategoriesPage = lazy(() => import('./pages/CategoriesPage'));
 const CategoryPage = lazy(() => import('./pages/CategoryPage'));
@@ -22,9 +21,9 @@ const CartPage = lazy(() => import('./pages/CartPage'));
 const PaymentPage = lazy(() => import('./pages/PaymentPage'));
 const PaymentSuccessPage = lazy(() => import('./pages/PaymentSuccessPage'));
 const ProfilePage = lazy(() => import('./pages/ProfilePage'));
-const AboutPage = lazy(() => import('./pages/AboutPage')); // Asumiendo que la tienes
-const ContactPage = lazy(() => import('./pages/ContactPage')); // Asumiendo que la tienes
-const BlogPage = lazy(() => import('./pages/BlogPage')); // Asumiendo que la tienes
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+const BlogPage = lazy(() => import('./pages/BlogPage'));
 const FaqPage = lazy(() => import('./pages/FaqPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
@@ -32,9 +31,9 @@ const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 const App: React.FC = () => {
   return (
     <Router>
-      <ScrollToTop /> {/* <--- AÑADE EL COMPONENTE AQUÍ */}
+      <ScrollToTop />
       <AuthProvider>
-        <ReservationProvider> {/* Asegúrate que este Provider esté correctamente implementado */}
+        <ReservationProvider>
           <CartProvider>
             <ToastContainer
               position="top-right"
@@ -54,16 +53,16 @@ const App: React.FC = () => {
                   <Route index element={<HomePage />} />
                   <Route path="categories" element={<CategoriesPage />} />
                   <Route path="category/:categoryId" element={<CategoryPage />} />
-                  <Route path="category/:categoryId/:subcategoryId" element={<CategoryPage />} /> {/* Para subcategorías */}
+                  <Route path="category/:categoryId/:subcategoryId" element={<CategoryPage />} />
                   <Route path="service/:serviceId" element={<ServiceDetailPage />} />
                   <Route path="search" element={<SearchResultsPage />} />
                   <Route path="cart" element={<CartPage />} />
                   <Route path="checkout" element={<PaymentPage />} />
                   <Route path="payment-success" element={<PaymentSuccessPage />} />
                   <Route path="profile" element={<ProfilePage />} />
-                  <Route path="about" element={<AboutPage />} /> {/* Asumiendo ruta */}
-                  <Route path="contact" element={<ContactPage />} /> {/* Asumiendo ruta */}
-                  <Route path="blog" element={<BlogPage />} /> {/* Asumiendo ruta */}
+                  <Route path="about" element={<AboutPage />} />
+                  <Route path="contact" element={<ContactPage />} />
+                  <Route path="blog" element={<BlogPage />} />
                   <Route path="faq" element={<FaqPage />} />
                   <Route path="*" element={<NotFoundPage />} />
                 </Route>
