792fa47278a4d94202e9383c0eb3c0f41955eb79
Jose Carlos Ramirez Lopez <60718287+lZood@users.noreply.github.com>
2025-05-28 01:30:39 -0700

    Update background color to white

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -15,7 +15,7 @@ const AboutPage: React.FC = () => {
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         </div>
         <div className="relative container-custom h-full flex flex-col justify-center text-white z-10">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4">Sobre Nosotros</h1>
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 text-white" >Sobre Nosotros</h1>
           <p className="text-lg md:text-xl max-w-2xl">
             Conoce a las mentes creativas detrás de CABETG Party Planner
           </p>
