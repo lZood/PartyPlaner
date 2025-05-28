542b78be0bd2ea6db5b447f00ad8ed0499e0d4db
Jose Carlos Ramirez Lopez <60718287+lZood@users.noreply.github.com>
2025-05-27 23:04:12 -0700

    Updated ScrollToTop.tsx

---
+++ src/utils/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+// src/components/utils/ScrollToTop.tsx
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null; // Este componente no renderiza nada visualmente
+};
+
+export default ScrollToTop;
